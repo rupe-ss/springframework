d86e6b92e6948c12e121d3a77a18be65afdca3a9
rupesss-09 <rupesss09@gmail.com>
2022-04-05 22:24:51 -0400

    Using getBean() method from Application Context

--- src/main/java/org/learning/App.java
+++ src/main/java/org/learning/App.java
@@ -1,13 +1,27 @@
 package org.learning;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        /*//So, I need a vehicle. How I get one. We can create can reference.
+        Car car = new Car(); //Here we got the Vehicle car but if we want bike in future then we have to change both side right and left.
+        //It looks not a big deal right now with small projects but things will be messy when its Enterprise code.
+        //How we solve thi problem, we can with Interface. But that will solve only left part we still need to change right side.
+        Vehicle vehicle = new Car();
+        vehicle.drive();
+        //But why even change a right side, to solve this we need a Spring Framework*/
+
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        /*
+        We are using ApplicationContext to create a Bean
+        getBean is method from ApplicationContext, but this method will give Object of Objects so we have to typeCase to Object to Vehicle
+         */
+        Vehicle vehicle = (Vehicle) context.getBean("vechile");
+        System.out.println("Hello World!");
     }
 }
